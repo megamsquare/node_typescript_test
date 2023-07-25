1d3543a2abe15a99904332f8cc8657a86684b13c
Michael <msquaremega@gmail.com>
2023-07-25 17:56:53 +0100

    trying to configure testing for redis

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,36 +1,59 @@
 import request from "supertest";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import { RedisMemoryServer } from "redis-memory-server";
 import mongoose from "mongoose";
 import app from "../../server";
 
 const mongoServer = new MongoMemoryServer();
 
+const redisServer = new RedisMemoryServer();
+
 describe("Auth", () => {
   beforeAll(async () => {
     await mongoServer.start();
     await mongoose.connect(mongoServer.getUri());
+    redisServer.start();
+    const host = await redisServer.getHost();
+    const port = await redisServer.getPort();
+
+    console.log(`redis memory host: ${host}, redis memory port: ${port}`);
   });
 
   afterAll(async () => {
     await mongoose.disconnect();
     await mongoose.connection.close();
     await mongoServer.stop();
+    await redisServer.stop();
   });
 
-  const userData = {
-    firstName: "Michael",
-    lastName: "Mensah",
-    username: "megamsquare",
-    email: "msquaremega@gmail.com",
-    password: "askme1",
+  const signUpData = {
+    firstName: "test",
+    lastName: "user",
+    username: "testUser",
+    email: "testuser@example.com",
+    password: "testUser",
+  };
+
+  const signInData = {
+    usernameOrEmail: "testUser",
+    password: "testUser",
   };
 
   describe("Auth Sign Up Test", () => {
     it("should register a new user", async () => {
       const signUpResponse = await request(app)
         .post("/api/v1/auth/signUp")
-        .send(userData);
+        .send(signUpData);
       expect(signUpResponse.status).toBe(201);
     });
   });
+
+//   describe("Auth Log In", () => {
+//     it("should log in user", async () => {
+//       const signInResponse = await request(app)
+//         .post("/api/v1/auth/signIn")
+//         .send(signInData);
+//       expect(signInResponse.status).toBe(200);
+//     });
+//   });
 });
