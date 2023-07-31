2ee23a128152cd57d151d3a9a3aed4df774e7926
Michael <msquaremega@gmail.com>
2023-07-31 21:40:37 +0100

    validate request from the sign up api

--- src/dto/obj/user.dto.ts
+++ src/dto/obj/user.dto.ts
@@ -1,26 +1,47 @@
 import { Request } from "express";
+import { ValidationChain, body } from "express-validator";
 
 export interface NewUser {
-    firstName: string;
-    lastName: string;
-    email: string;
-    username: string;
-    password: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  password: string;
 }
 
 export interface UserRequest extends Request {
-    user?: { userId: string; role: string[] };
+  user?: { userId: string; role: string[] };
 }
 
 export interface UpdateUser {
-    userId: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    username: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
 }
 
 export interface UpdatePassword {
-    userId: string;
-    password: string;
+  userId: string;
+  password: string;
 }
+
+export const validateNewUser: ValidationChain[] = [
+  body("firstName")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("first name is required"),
+  body("lastName")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("last name is required"),
+  body("email").trim().isEmail().withMessage("email is required"),
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("username is required"),
+  body("password")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("password is required"),
+];
