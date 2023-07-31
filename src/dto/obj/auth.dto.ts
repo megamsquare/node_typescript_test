1062cea8f3f7d6c9b6f04f071ebb97b5fe250f7f
Michael <msquaremega@gmail.com>
2023-07-31 21:25:28 +0100

    validate request from the api

--- src/dto/obj/auth.dto.ts
+++ src/dto/obj/auth.dto.ts
@@ -1,21 +1,34 @@
+import { ValidationChain, body } from "express-validator";
+
 export interface IsRefresh {
-    check: boolean;
-    refreshToken: string;
-    roles: string[] | undefined;
+  check: boolean;
+  refreshToken: string;
+  roles: string[] | undefined;
 }
 
 export interface LoginInfo {
-    usernameOrEmail: string;
-    password: string;
+  usernameOrEmail: string;
+  password: string;
 }
 
 export interface AccessTokenCheck {
-    header: string | undefined;
-    checkExpire: boolean;
+  header: string | undefined;
+  checkExpire: boolean;
 }
 
 export interface ForgottenPassword {
-    email: string;
-    oldPassword: string;
-    newPassword: string;
+  email: string;
+  oldPassword: string;
+  newPassword: string;
 }
+
+export const validateSignUp: ValidationChain[] = [
+  body("usernameOrEmail")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Email or Username is required"),
+  body("password")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Password is required"),
+];
