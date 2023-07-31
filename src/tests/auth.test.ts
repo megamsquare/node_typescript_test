f0986ad5bca38482e317f3b1e3b1b701ff5c6113
Michael <msquaremega@gmail.com>
2023-07-31 20:33:43 +0100

    comment out redis settings in test

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -19,28 +19,28 @@ describe("Auth", () => {
   beforeAll(async () => {
     await mongoServer.start();
     await mongoose.connect(mongoServer.getUri());
-    redisServer.start();
-    const host = await redisServer.getHost();
-    const port = await redisServer.getPort();
+    // redisServer.start();
+    // const host = await redisServer.getHost();
+    // const port = await redisServer.getPort();
 
-    const redis_client = createClient({
-      legacyMode: true,
-      socket: {
-        host: host,
-        port: port,
-      },
-    });
+    // const redis_client = createClient({
+    //   legacyMode: true,
+    //   socket: {
+    //     host: host,
+    //     port: port,
+    //   },
+    // });
 
-    redis_client.on("error", (error) =>
-      console.error(`Error from connecting to redis: ${error}`)
-    );
+    // redis_client.on("error", (error) =>
+    //   console.error(`Error from connecting to redis: ${error}`)
+    // );
 
-    try {
-        await redis_client.connect();
-        console.log('Successfully connect to Redis');
-    } catch (err) {
-        console.error(`Redis connection error: ${err}`);
-    }
+    // try {
+    //     await redis_client.connect();
+    //     console.log('Successfully connect to Redis');
+    // } catch (err) {
+    //     console.error(`Redis connection error: ${err}`);
+    // }
 
     // console.log(`redis memory host: ${host}, redis memory port: ${port}`);
     // await DB.caching.connect_redis();
@@ -50,7 +50,7 @@ describe("Auth", () => {
     await mongoose.disconnect();
     await mongoose.connection.close();
     await mongoServer.stop();
-    await redisServer.stop();
+    // await redisServer.stop();
   });
 
   const signUpData = {
