b9a2b3387b3ba2df9c549ac34868fdd2cf8ce025
Michael <msquaremega@gmail.com>
2023-08-03 18:04:14 +0100

    validate update user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import userController from '../controllers/user.controller';
-import { validateNewUser } from '../dto/obj/user.dto';
+import { validateNewUser, validateUpdateUser } from '../dto/obj/user.dto';
 import Middleware from '../middleware';
 
 const routers =  express.Router();
 
 routers.post('/createUser', validateNewUser, Middleware.ValidationResponse.validateResult, userController.createUser);
-routers.put('/updateUser', userController.updateUser)
+routers.put('/updateUser', validateUpdateUser, Middleware.ValidationResponse.validateResult, userController.updateUser)
 
 // Export the rtouter
 export default routers;
