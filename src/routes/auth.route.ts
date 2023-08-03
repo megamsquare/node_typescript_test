446f196494f804418fc4885cbc18d9f52edcbdb7
Michael <msquaremega@gmail.com>
2023-08-03 17:58:23 +0100

    validate sign up input change

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import Auth_controller from '../controllers/auth.controller';
 import { validateSignIn } from '../dto/obj/auth.dto';
 import Middleware from '../middleware';
+import { validateNewUser } from '../dto/obj/user.dto';
 
 const router = express.Router();
 
-router.post('/signUp', Middleware.ValidationResponse.validateResult, Auth_controller.signUp);
+router.post('/signUp', validateNewUser, Middleware.ValidationResponse.validateResult, Auth_controller.signUp);
 router.post('/signIn', validateSignIn, Middleware.ValidationResponse.validateResult, Auth_controller.signIn);
 router.post('/refresh', Auth_controller.refreshToken);
 router.post('/forgottenPassword', Auth_controller.forgot_password);
