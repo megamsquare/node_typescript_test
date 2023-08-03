8c751749aacc0f6447eb72cf1335c1e8b612c5f9
Michael <msquaremega@gmail.com>
2023-08-03 17:56:56 +0100

    validate sign in input

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,8 +5,8 @@ import Middleware from '../middleware';
 
 const router = express.Router();
 
-router.post('/signUp', validateSignIn, Middleware.ValidationResponse.validateResult, Auth_controller.signUp);
-router.post('/signIn', Middleware.ValidationResponse.validateResult, Auth_controller.signIn);
+router.post('/signUp', Middleware.ValidationResponse.validateResult, Auth_controller.signUp);
+router.post('/signIn', validateSignIn, Middleware.ValidationResponse.validateResult, Auth_controller.signIn);
 router.post('/refresh', Auth_controller.refreshToken);
 router.post('/forgottenPassword', Auth_controller.forgot_password);
 
