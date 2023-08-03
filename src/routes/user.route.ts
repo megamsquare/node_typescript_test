7e8128dbcc57626aa53bab3f116d7eda52a01aae
Michael <msquaremega@gmail.com>
2023-08-03 19:20:52 +0100

    file rearrangement

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,22 @@
-import express from 'express';
-import userController from '../controllers/user.controller';
-import { validateNewUser, validateUpdateUser } from '../dto/obj/user.dto';
-import Middleware from '../middleware';
+import express from "express";
+import userController from "../controllers/user.controller";
+import { validateNewUser, validateUpdateUser } from "../dto/obj/user.dto";
+import Middleware from "../middleware";
 
-const routers =  express.Router();
+const routers = express.Router();
 
-routers.post('/createUser', validateNewUser, Middleware.ValidationResponse.validateResult, userController.createUser);
-routers.put('/updateUser', validateUpdateUser, Middleware.ValidationResponse.validateResult, userController.updateUser)
+routers.post(
+  "/createUser",
+  validateNewUser,
+  Middleware.ValidationResponse.validateResult,
+  userController.createUser
+);
+routers.put(
+  "/updateUser",
+  validateUpdateUser,
+  Middleware.ValidationResponse.validateResult,
+  userController.updateUser
+);
 
 // Export the rtouter
 export default routers;
