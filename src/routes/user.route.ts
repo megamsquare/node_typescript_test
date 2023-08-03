35b531536f9de36cef1edc5e91ea60bc59fa850e
Michael <msquaremega@gmail.com>
2023-08-03 18:01:46 +0100

    validate create new user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import userController from '../controllers/user.controller';
+import { validateNewUser } from '../dto/obj/user.dto';
+import Middleware from '../middleware';
 
 const routers =  express.Router();
 
-routers.post('/createUser', userController.createUser);
+routers.post('/createUser', validateNewUser, Middleware.ValidationResponse.validateResult, userController.createUser);
 routers.put('/updateUser', userController.updateUser)
 
 // Export the rtouter
